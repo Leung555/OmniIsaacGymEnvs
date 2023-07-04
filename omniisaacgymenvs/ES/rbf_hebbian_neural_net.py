7f214a1ea5045dc0942e29df8eede2c1c9b3ac6f
Binggwong Leung <binggwong.l_s17@vistec.ac.th>
2023-07-04 18:17:54 +0200

    deplaoy RBF for training
    - fast train result

--- omniisaacgymenvs/ES/rbf_hebbian_neural_net.py
+++ omniisaacgymenvs/ES/rbf_hebbian_neural_net.py
@@ -1,92 +1,106 @@
 import numpy as np
 import torch
+from math import cos, sin, tanh
 
 def WeightStand(w, eps=1e-5):
 
-    mean = torch.mean(input=w, dim=[0,1], keepdim=True)
-    var = torch.var(input=w, dim=[0,1], keepdim=True)
+    mean = torch.mean(input=w, dim=[1,2], keepdim=True)
+    var = torch.var(input=w, dim=[1,2], keepdim=True)
 
-    w = (w - mean) / torch.sqrt(var + eps)
+    w = (w - mean) / torch.sqrt(var)
 
     return w
 
-
 class RBFHebbianNet:
-    def __init__(self, num_output, num_basis=10):
+    def __init__(self, POPSIZE, num_output, num_basis=10):
         """
         sizes: [input_size, hid_1, ..., output_size]
         """
+        self.POPSIZE = POPSIZE
         # cpg params
-        # self.omega = 0.01*np.pi
-        # self.o1, self.o2 = 0.01, 0.01
-        # # Rbf network
-        # input_range = (-1, 1)
-        # self.num_basis = num_basis
-        # self.num_output = num_output
-        # self.centers = np.linspace(input_range[0], input_range[1], num=num_basis)
-        # self.variance = 1/0.04
-        # self.weights = np.random.randn(num_output, num_basis, )
+        self.omega = 0.01*np.pi
+        self.alpha = 1.01
+        # self.o1 = torch.Tensor.repeat(torch.Tensor([0.00]), POPSIZE).unsqueeze(1)
+        # self.o2 = torch.Tensor.repeat(torch.Tensor([0.18]), POPSIZE).unsqueeze(1)
 
-        # self.weights = [torch.Tensor(sizes[i], sizes[i + 1]).uniform_(-1.0,1.0)
-        #                     for i in range(len(sizes) - 1)]
-          
+        self.O = torch.Tensor([[0.01, 0.18]]).expand(POPSIZE, 2).cuda()
+        # Rbf network
+        self.input_range = (-1, 1)
+        self.num_basis = num_basis
+        self.num_output = num_output
+        self.centers = torch.linspace(self.input_range[0], 
+                                      self.input_range[1], num_basis).cuda()
+        self.variance = 1/0.04
+        self.weights = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        self.A = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        self.B = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        self.C = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        self.D = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        self.lr = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        # print(self.weights)
+        self.W = torch.Tensor([[ cos(self.omega) ,  -sin(self.omega)], 
+                                [ sin(self.omega) ,  cos(self.omega)]]).cuda()
 
 
-    def forward(self):
+    def forward(self, pre):
 
         with torch.no_grad():
-            pre = torch.from_numpy(pre)
-            """
-            pre: (n_in, )
-            """
-            # print('---- forward ----------------------------------')
-            # print('self.test: ', self.weights[0])
-            # print('----------------------------------------------')
-            for i in range(len(self.weights)):
-                # W = self.weights[i]
-                # print('pre: ', pre)
-                # print('W: ', self.weights[i])
-                post = torch.tanh(pre @ self.weights[i].float())
-                # post = torch.tanh(pre @ W.double())
+            self.O = torch.tanh(self.alpha*torch.matmul(self.O, self.W))
+            post = torch.matmul(torch.exp(-self.variance*torch.sum((self.O.reshape(self.POPSIZE, 2, 1).
+                    expand(self.POPSIZE,2,self.num_basis) - self.centers) ** 2, dim=1)).double(), self.weights.double())[:, 0]
+            # post = torch.matmul(a.double(), self.weights.double())[:, 0]
+        return post.float().detach()
 
-                pre = post
-            
-            # for i, W in enumerate(self.weights):
-            #     # W = W.cuda()
-            #     # print('pre: ', i, pre)
-            #     # print('W: ', i, W)
-            #     post = torch.tanh(pre @ W.float())
-            #     # post = torch.tanh(pre @ W.double())
+    def hebbian_update(self, hid_num ,weights, pre, post, A, B, C, D, lr):
 
-            #     pre = post
-            # print('post: ', post)
-            # print('post: ', post.detach())
+        i = self.one_array[hid_num] * pre.unsqueeze(2)
+        j = post.unsqueeze(2).expand(-1,-1, weights.shape[1]).transpose(1,2)
+        ij = i * j
 
-        return post.detach()
+        new_weights = weights + lr * (A*ij + B*i + C*j + D)
+        # print('weights update: ', weights)
+        new_weights = WeightStand(weights)        
+        
+        return new_weights
 
     def get_params(self):
-        p = torch.cat([ params.flatten() for params in self.weights] )
-
+        p = torch.cat([ params.flatten() for params in self.A]  
+                +[ params.flatten() for params in self.B] 
+                +[ params.flatten() for params in self.C]
+                +[ params.flatten() for params in self.D]
+                +[ params.flatten() for params in self.lr]
+                )
         return p.cpu().flatten().numpy()
 
-
+    def get_params_a_model(self):
+        p = torch.cat([ params[0].flatten() for params in self.A]  
+                +[ params[0].flatten() for params in self.B] 
+                +[ params[0].flatten() for params in self.C]
+                +[ params[0].flatten() for params in self.D]
+                +[ params[0].flatten() for params in self.lr]
+                )
+        return p.flatten().numpy()
+        
     def set_params(self, flat_params):
         flat_params = torch.from_numpy(flat_params)
 
         m = 0
+        for i, hebb_A in enumerate(self.A):
+            pop, a, b = hebb_A.shape
+            self.A[i] = flat_params[:, m:m + a * b].reshape(pop, a, b).cuda()
+            m += a * b 
+        
         # print('---- set_params ---------------------------------')
         # print('self.test: ', self.weights[0])
         # print('----------------------------------------------')
-        for i, w in enumerate(self.weights):
-            # print('w: ', w)
-            a, b = w.shape
-            self.weights[i] = flat_params[m:m + a * b].reshape(a, b)
-            # self.weights[i] = self.weights[i].cuda()
-            m += a * b 
+        # print('w: ', w)
+        basis, popsize, num_out = self.weights.shape
+        self.weights = flat_params.reshape(basis, popsize, num_out).cuda()
+        # self.weights[i] = self.weights[i].cuda()
         # print('---- set_params ouput ---------------------------------')
         # print('self.test: ', self.weights[0])
         # print('----------------------------------------------')
 
 
     def get_weights(self):
-        return [w for w in self.weights]
+        return self.weights
--- omniisaacgymenvs/ES/rbf_neural_net.py
+++ omniisaacgymenvs/ES/rbf_neural_net.py
@@ -14,15 +14,16 @@ class RBFNet:
         # self.o1 = torch.Tensor.repeat(torch.Tensor([0.00]), POPSIZE).unsqueeze(1)
         # self.o2 = torch.Tensor.repeat(torch.Tensor([0.18]), POPSIZE).unsqueeze(1)
 
-        self.O = torch.Tensor([[0.01, 0.18]]).expand(POPSIZE, 2)
+        self.O = torch.Tensor([[0.01, 0.18]]).expand(POPSIZE, 2).cuda()
         # Rbf network
         self.input_range = (-1, 1)
-        self.num_basis = 10
-        self.num_output = 1
+        self.num_basis = num_basis
+        self.num_output = num_output
         self.centers = torch.linspace(self.input_range[0], 
                                       self.input_range[1], num_basis).cuda()
         self.variance = 1/0.04
         self.weights = torch.randn(self.POPSIZE, self.num_basis, self.num_output,).cuda()
+        # print(self.weights)
         self.W = torch.Tensor([[ cos(self.omega) ,  -sin(self.omega)], 
                                 [ sin(self.omega) ,  cos(self.omega)]]).cuda()
 
@@ -31,10 +32,10 @@ class RBFNet:
 
         with torch.no_grad():
             self.O = torch.tanh(self.alpha*torch.matmul(self.O, self.W))
-            a = torch.exp(-self.variance*torch.sum((self.O.reshape(self.POPSIZE, 2, 1).
-                    expand(self.POPSIZE,2,self.num_basis) - self.centers) ** 2, dim=1))
-            post = torch.matmul(a, self.weights)[:, 0]
-        return post.detach()
+            post = torch.matmul(torch.exp(-self.variance*torch.sum((self.O.reshape(self.POPSIZE, 2, 1).
+                    expand(self.POPSIZE,2,self.num_basis) - self.centers) ** 2, dim=1)).double(), self.weights.double())[:, 0]
+            # post = torch.matmul(a.double(), self.weights.double())[:, 0]
+        return post.float().detach()
 
     def get_params(self):
         p = torch.cat([ params.flatten() for params in self.weights] )
@@ -42,7 +43,7 @@ class RBFNet:
         return p.cpu().flatten().numpy()
 
     def get_params_a_model(self):
-        p = torch.cat([ params.flatten() for params in self.weights[:, 0]] )
+        p = torch.cat([ params.flatten() for params in self.weights[0]] )
 
         return p.cpu().flatten().numpy()
     
@@ -53,8 +54,8 @@ class RBFNet:
         # print('self.test: ', self.weights[0])
         # print('----------------------------------------------')
         # print('w: ', w)
-        basis, popsize, num_out = self.weights.shape
-        self.weights = flat_params.reshape(basis, popsize, num_out).cuda()
+        popsize, basis, num_out = self.weights.shape
+        self.weights = flat_params.reshape(popsize, basis, num_out).cuda()
         # self.weights[i] = self.weights[i].cuda()
         # print('---- set_params ouput ---------------------------------')
         # print('self.test: ', self.weights[0])
